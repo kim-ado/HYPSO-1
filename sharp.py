bb6d0768d7142f10fd1f5c58c04cc98fc72e6160
sivertba <sivert.bakken@gmail.com>
2022-12-13 13:37:04 +0100

    Expand sharp class to include metrics and cs method

--- sharp.py
+++ sharp.py
@@ -1,6 +1,8 @@
 import os
-import numpy as np
+
 import matplotlib.pyplot as plt
+import numpy as np
+import spectral.io.envi as envi
 
 
 class h1data:
@@ -28,8 +30,8 @@ class h1data:
         self.spec_coefficients = self.set_spectral_coefficients(path=None)
 
         # Load the raw data
-        self.raw_cube = self.get_raw_cube()
-        self.l1a_cube = self.calibrate_cube()
+        self.raw_cube = self.get_raw_cube()  # consider initializing with None
+        self.l1a_cube = self.calibrate_cube()  # consider initializing with None
         self.wavelengths = None
 
     def get_metainfo(self, top_folder_name: str) -> dict:
@@ -53,7 +55,7 @@ class h1data:
         else:
             raise ValueError("No folder with metadata found.")
 
-        # combine top_foldr_name and raw_folder to get the path to the raw data
+        # combine top_folder_name and raw_folder to get the path to the raw data
         config_file_path = os.path.join(
             top_folder_name, raw_folder, "capture_config.ini")
 
@@ -162,6 +164,7 @@ class h1data:
             radiometric_coeffs = None
 
         self.radiometric_coefficients = radiometric_coeffs
+        self.calibrate_cube()
         return radiometric_coeffs
 
     def get_raw_cube(self) -> np.ndarray:
@@ -194,7 +197,7 @@ class h1data:
 
         return frame_calibrated
 
-    def calibrate_cube(self):
+    def calibrate_cube(self) -> np.ndarray:
         """Calibrate the raw data cube."""
 
         background_value = self.info['background_value']
@@ -227,22 +230,188 @@ class h1data:
         plt.imshow(np.rot90(self.l1a_cube[:, ::10, 20]), aspect=3)
 
 
-class sharpness_assessment:
-    def __init__(self, cube: h1data):
-        self.cube = cube
+class sharp:
+    def __init__(self):
+        self.initial_cube = None
+        self.refrence_cube = None
 
-        self.brisque_raw = self.get_brisque_raw()
-        self.brisque_l1a = self.get_brisque_l1a()
+        self.sharpened_cube = None
+        self.sharpest_band_index = None
 
-    def get_brisque_raw(self) -> np.ndarray:
-        """Get the brisque score for the raw data cube."""
-        brisque_raw = brisque(self.cube.raw_cube)
-        return brisque_raw
+        # metrics - no refrence
+        self.brisque = None
 
-    def get_brisque_l1a(self) -> np.ndarray:
-        """Get the brisque score for the l1a data cube."""
-        brisque_l1a = brisque(self.cube.l1a_cube)
-        return brisque_l1a
+        # metrics - full refrence
+        self.sam = None
+
+    def sharpen_cs(self):
+        """Sharpen the cube using component substitution method."""
+        self.sharpened_cube = component_subtitution(
+            self.initial_cube, self.sharpest_band_index)
+
+    @classmethod
+    def fromh1data(cls, h1data: h1data):
+        """Create a sharp object from a h1data object.
+
+        Args:
+            h1data (h1data): h1data object.
+
+        Returns:
+            sharp: sharp object.
+        """
+        sharp_obj = cls()
+
+        if h1data.l1a_cube is None:
+            try:
+                h1data.calibrate_cube()
+            except:
+                sharp_obj.initial_cube = h1data.raw_cube
+        else:
+            sharp_obj.initial_cube = h1data.l1a_cube
+
+        # Initialise the sharpest band index to the middle band
+        sharp_obj.sharpest_band_index = np.shape(
+            sharp_obj.initial_cube)[3] // 2
+
+        sharp_obj.evaluate()
+
+        return sharp_obj
+
+    @classmethod
+    def fromenvifile(cls, headerfile_path: str, datafile_path: str = None):
+        """Create a sharp object from a envi file.
+
+        Args:
+            path (str): Path to the envi file.
+
+        Returns:
+            sharp: sharp object.
+        """
+        sharp_obj = cls()
+
+        if ".hdr" not in headerfile_path:
+            raise ValueError("Path to envi header file must end with .hdr")
+        elif datafile_path is None:
+            # assume data file is in same folder as header file
+            try:
+                datafile_path = headerfile_path.replace(".hdr", ".bip")
+            except:
+                raise ValueError(
+                    "Function expects data to be in .bip format")
+
+        temp_obj = envi.open(headerfile_path, datafile_path)
+        sharp_obj.initial_cube = temp_obj.load()
+        sharp_obj.get_no_refrence_metric()
+
+        return sharp_obj
+
+    def get_brisque(self) -> np.ndarray:
+        """Get the brisque metric.
+
+        Returns:
+            float: The brisque metric.
+        """
+        self.brisque = brisque(self.initial_cube)
+        return self.brisque
+
+    def get_sam(self) -> np.ndarray:
+        """Get the sam metric.
+
+        Returns:
+            float: The sam metric.
+        """
+        self.sam = sam(self.initial_cube, self.refrence_cube)
+        return self.sam
+
+    def load_refrence_cube(self, refrence_cube: np.ndarray) -> np.ndarray:
+        """Load a refrence cube.
+
+        Args:
+            refrence_cube (np.ndarray): The refrence cube.
+
+        Returns:
+            np.ndarray: The refrence cube.
+        """
+        self.refrence_cube = refrence_cube
+        return self.refrence_cube
+
+    def evaluate(self) -> None:
+        """Evaluate the sharpness of the initial cube.
+
+        Args:
+            refrence_cube (np.ndarray): The refrence cube.
+        """
+
+        if self.initial_cube == None:
+            raise ValueError("No initial cube loaded")
+        elif self.refrence_cube == None:
+            self.brisque = self.get_brisque()
+        else:
+            self.brisque = self.get_brisque()
+            self.sam = self.get_sam()
+
+
+def sam(image: np.ndarray, refrence_image: np.ndarray) -> np.ndarray:
+    """Calculate the spectral angle (SAM) metric.
+
+    Args:
+        image (np.ndarray): The image to be evaluated.
+        refrence_image (np.ndarray): The refrence image.
+
+    Returns:
+        np.ndarray: The sam metric for each wavelength.
+    """
+    # check if image and refrence_image have same shape
+    if image.shape != refrence_image.shape:
+        raise ValueError("image and refrence_image must have same shape")
+
+    sam_metric = np.zeros(image.shape[2])
+    for i in range(image.shape[2]):
+        # TODO: add sam score for each wavelength
+        sam_metric[i] = np.mean((image[:, :, i]) + refrence_image[:, :, i])
+
+    return sam_metric
+
+
+def brisque(image: np.ndarray) -> np.ndarray:
+    """Calculate the brisque metric.
+
+    Args:
+        image (np.ndarray): The image to be evaluated.
+
+    Returns:
+        np.ndarray: The brisque metric for each wavelength.
+    """
+    brisque_metric = np.zeros(image.shape[2])
+    for i in range(image.shape[2]):
+        # TODO: add brisque score for each wavelength
+        brisque_metric[i] = np.mean((image[:, :, i]))
+
+    return brisque_metric
+
+
+def component_subtitution(image: np.ndarray, sharpest_band_index: int = None) -> np.ndarray:
+    """Perform component substitution on an image.
+
+    Args:
+        image (np.ndarray): The image to be sharpened.
+        sharpest_band_index (int, optional): The sharpest band. Defaults to None.
+
+    Returns:
+        np.ndarray: The sharpened image.
+    """
+    if sharpest_band_index is None:
+        sharpest_band_index = image.shape[2] // 2
+
+    # TODO: do PCA on image
+    # TODO: histogram match first PCA component to the sharpest band
+    # TODO: replace first component with the sharpest band
+
+    sharpened_image = np.zeros(image.shape)
+    for i in range(image.shape[2]):
+        sharpened_image[:, :, i] = image[:, :, sharpest_band_index]
+
+    return sharpened_image
 
 
 def main():
