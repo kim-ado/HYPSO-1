d4b13de731525979a56b6e165d0ce2d395d43553
Kim <mikdoan99@gmail.com>
2024-03-26 20:57:31 +0100

    binary search function

--- blur.py
+++ blur.py
@@ -83,10 +83,35 @@ class blurCube():
             self.current_fwhm.append(fwhm)
             cv2.gaussianBlur(self.cube[i], (5, 5), self.guessed_sigma[i])
 
-            while abs(self.is_pairwise_matching(self.current_fwhm[i], self.desired_fwhm[i])):
-                self.guessed_sigma[i] 
+            while abs(self.current_fwhm[i] - self.desired_fwhm[i])> 0.03 :
+                self.guessed_sigma[i] = 
 
 
+    def binary_search(self, arr, low, high, x):
+    
+        # Check base case
+        if high >= low:
+    
+            mid = (high + low) // 2
+    
+            # If element is present at the middle itself
+            if arr[mid] == x:
+                return mid
+    
+            # If element is smaller than mid, then it can only
+            # be present in left subarray
+            elif arr[mid] > x:
+                return self.binary_search(arr, low, mid - 1, x)
+    
+            # Else the element can only be present in right subarray
+            else:
+                return self.binary_search(arr, mid + 1, high, x)
+    
+        else:
+            # Element is not present in the array
+            return -1
+
+    
     def is_pairwise_matching(a, b):
         return all(x == y for x, y in zip(a, b))
     
