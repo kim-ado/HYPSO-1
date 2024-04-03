564988df388cf6cabab848f9fa3ed6a9275b7b49
Kim Anh Doan <mikdoan99@gmail.com>
2024-04-03 16:40:19 +0200

    first test of download stuff

--- hico_batch_scraper.py
+++ hico_batch_scraper.py
@@ -1,8 +1,8 @@
 import urllib
 import os
-from http.cookiejar import CookieJar
 from html.parser import HTMLParser
 import requests
+import bz2
 
 '''
  This script, NSIDC_parse_HTML_BatchDL.py, defines an HTML parser to scrape data files from 
@@ -84,6 +84,10 @@ class MyHTMLParser(HTMLParser):
                         self.lasttag = tag
                         if value.endswith('/'):  # Check if the link is a subdirectory
                             self.dataList.append(value)  # Store the subdirectory
+    def reset_state(self):
+        self.inLink = False
+        self.dataList = []
+        self.Counter = 0
                     
     def handle_endtag(self, tag):
             if tag == 'table':
@@ -94,94 +98,38 @@ class MyHTMLParser(HTMLParser):
             if self.lasttag == 'a' and self.inLink and data.strip():
                 self.dataList.append(data)
         
-parser = MyHTMLParser() 
 
 # Define function for batch downloading
-def BatchJob(Files, cookie_jar):
+def BatchJob(Files, cookie_jar, url, folder_name):
     for dat in Files:
-        print("downloading: ", dat)
-        JobRequest = urllib.request.Request(url+dat)
-        JobRequest.add_header('cookie', str(cookie_jar)) # Pass the saved cookie into additional HTTP request
-        JobResponse = urllib.request.urlopen(JobRequest)
+        if dat.endswith('.bz2'):  # Check if the file is a .bz2 file
+            print("downloading: ", dat)
+            JobRequest = urllib.request.Request(url+dat)
+            JobRequest.add_header('cookie', str(cookie_jar)) # Pass the saved cookie into additional HTTP request
+            JobResponse = urllib.request.urlopen(JobRequest)
 
-        JobRedirect_url = JobResponse.geturl() + '&app_type=401'
-        # Request the resource at the modified redirect url
-        Request = urllib.request.Request(JobRedirect_url)
-        Response = urllib.request.urlopen(Request)
-        f = open(dat, 'wb')
-        f.write(Response.read())
-        f.close()
-        Response.close()
-    print("Files downloaded to: ", os.path.dirname(os.path.realpath(__file__)))
+            JobRedirect_url = JobResponse.geturl() + '&app_type=401'
+            # Request the resource at the modified redirect url
+            Request = urllib.request.Request(JobRedirect_url)
+            Response = urllib.request.urlopen(Request)
+            bz2_file = folder_name + '/' + dat
+            with open(bz2_file, 'wb') as f:  # Add the path to the hico_data folder
+                f.write(Response.read())
+            Response.close()
+
+            # Unzip the .bz2 file
+            newfilepath = bz2_file[:-4]
+            with open(newfilepath, 'wb') as new_file, bz2.BZ2File(bz2_file, 'rb') as file:
+                for data in iter(lambda : file.read(100 * 1024), b''):
+                    new_file.write(data)
+
+            # Delete the .bz2 file
+            os.remove(bz2_file)
+
+    print("Files downloaded and extracted to: ", os.path.dirname(os.path.realpath(__file__)) + '/' + folder_name)
 
 
-#===============================================================================
-# The following code block is used for HTTPS authentication
-#===============================================================================
 
-# The user credentials that will be used to authenticate access to the data
-username = "kimado"
-password = "&82m%wPhE8-9.C+"
-
-# The FULL url of the directory which contains the files you would like to bulk download
-url = "https://oceandata.sci.gsfc.nasa.gov/directdataaccess/Level-1B/HICO/2014" # Example URL
-
-# Create a password manager to deal with the 401 reponse that is returned from
-# Earthdata Login
- 
-password_manager = urllib.request.HTTPPasswordMgrWithDefaultRealm()
-password_manager.add_password(None, "https://urs.earthdata.nasa.gov", username, password)
- 
-# Create a cookie jar for storing cookies. This is used to store and return
-# the session cookie given to use by the data server (otherwise it will just
-# keep sending us back to Earthdata Login to authenticate).  Ideally, we
-# should use a file based cookie jar to preserve cookies between runs. This
-# will make it much more efficient.
- 
-cookie_jar = CookieJar()
-
-# Install all the handlers.
-opener = urllib.request.build_opener(
-    urllib.request.HTTPBasicAuthHandler(password_manager),
-    #urllib.request.HTTPHandler(debuglevel=1),    # Uncomment these two lines to see
-    #urllib.request.HTTPSHandler(debuglevel=1),   # details of the requests/responses
-    urllib.request.HTTPCookieProcessor(cookie_jar))
-urllib.request.install_opener(opener)
- 
-# Create and submit the requests. There are a wide range of exceptions that
-# can be thrown here, including HTTPError and URLError. These should be
-# caught and handled.
-
-#===============================================================================
-# Open a requeset to grab filenames within a directory. Print optional
-#===============================================================================
-
-DirRequest = urllib.request.Request(url)
-DirResponse = urllib.request.urlopen(DirRequest)
-
-# Get the redirect url and append 'app_type=401'
-# to do basic http auth
-DirRedirect_url = DirResponse.geturl()
-DirRedirect_url += '&app_type=401'
-
-# Request the resource at the modified redirect url
-DirRequest = urllib.request.Request(DirRedirect_url)
-DirResponse = urllib.request.urlopen(DirRequest)
-
-DirBody = DirResponse.read()
-
-# Uses the HTML parser defined above to pring the content of the directory containing data
-parser.feed(str(DirBody))
-Files = parser.dataList
-
-# Display the contents of the python list declared in the HTMLParser class
-# print Files #Uncomment to print a list of the files
-
-#===============================================================================
-# Call the function to download all files in url
-#===============================================================================
-
-BatchJob(Files, cookie_jar) # Comment out to prevent downloading to your working directory
 
 
 
